118f280403ec727c47347ae9dc58a30ea277cf84
Luke Lu <luke_lmn@berkeley.edu>
2012-10-15 13:42:21 -0700

    Took the selection service stuff I found and I stuffed it into our plugin. Take a look. When you click something in the explorer, it'll appear in our plugin!

--- trunk/plugin/src/plugin/views/EduRideJunitView.java
+++ trunk/plugin/src/plugin/views/EduRideJunitView.java
@@ -4,14 +4,28 @@ package plugin.views;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.ui.model.WorkbenchLabelProvider;
 import org.eclipse.ui.part.*;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.part.PageBook;
+import org.eclipse.ui.part.ViewPart;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IMarkSelection;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.text.TextViewer;
 import org.eclipse.jface.viewers.*;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.jface.action.*;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.*;
 import org.eclipse.swt.widgets.Menu;
-import org.eclipse.swt.SWT;
 
 
 /**
@@ -87,14 +101,64 @@ public class EduRideJunitView extends ViewPart {
 	public EduRideJunitView() {
 	}
 
+
+	private PageBook pagebook;
+	private TableViewer tableviewer;
+	private TextViewer textviewer;
 	
+	// the listener we register with the selection service 
+	private ISelectionListener listener = new ISelectionListener() {
+		public void selectionChanged(IWorkbenchPart sourcepart, ISelection selection) {
+			// we ignore our own selections
+			if (sourcepart != EduRideJunitView.this) {
+			    showSelection(sourcepart, selection);
+			}
+		}
+	};
+	
+	/**
+	 * Shows the given selection in this view.
+	 */
+	public void showSelection(IWorkbenchPart sourcepart, ISelection selection) {
+		setContentDescription(sourcepart.getTitle() + " (" + selection.getClass().getName() + ")");
+		if (selection instanceof IStructuredSelection) {
+			IStructuredSelection ss = (IStructuredSelection) selection;
+			showItems(ss.toArray());
+		}
+		if (selection instanceof ITextSelection) {
+			ITextSelection ts  = (ITextSelection) selection;
+			showText(ts.getText());
+		}
+		if (selection instanceof IMarkSelection) {
+			IMarkSelection ms = (IMarkSelection) selection;
+			try {
+			    showText(ms.getDocument().get(ms.getOffset(), ms.getLength()));
+			} catch (BadLocationException ble) { }
+		}
+	}
+	
+	private void showItems(Object[] items) {
+		tableviewer.setInput(items);
+		pagebook.showPage(tableviewer.getControl());
+	}
+	
+	private void showText(String text) {
+		textviewer.setDocument(new Document(text));
+		pagebook.showPage(textviewer.getControl());
+	}
+
+	public void dispose() {
+		// important: We need do unregister our listener when the view is disposed
+		getSite().getWorkbenchWindow().getSelectionService().removeSelectionListener(listener);
+		super.dispose();
+	}
+		
 	/**
 	 * This is a callback that will allow us
 	 * to create the viewer and initialize it.
 	 */
 	public void createPartControl(Composite parent) {
 		//viewer = new TableViewer(parent, SWT.MULTI | SWT.H_SCROLL | SWT.V_SCROLL);
-		
 		viewer = new TableViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL | SWT.MULTI | SWT.FULL_SELECTION);
 		final Table table = viewer.getTable();
 		assignmentColumn = new TableColumn(table, SWT.LEFT);
@@ -119,7 +183,24 @@ public class EduRideJunitView extends ViewPart {
 		makeActions();
 		hookContextMenu();
 		hookDoubleClickAction();
-		contributeToActionBars();
+		contributeToActionBars();		
+		
+		// the PageBook allows simple switching between two viewers
+		pagebook = new PageBook(parent, SWT.NONE);
+		
+		tableviewer = new TableViewer(pagebook, SWT.NONE);
+		tableviewer.setLabelProvider(new WorkbenchLabelProvider());
+		tableviewer.setContentProvider(new ArrayContentProvider());
+		
+		// we're cooperative and also provide our selection
+		// at least for the tableviewer
+		getSite().setSelectionProvider(tableviewer);
+		
+		textviewer = new TextViewer(pagebook, SWT.H_SCROLL | SWT.V_SCROLL);
+		textviewer.setEditable(false);
+		
+		getSite().getWorkbenchWindow().getSelectionService().addSelectionListener(listener);
+		
 	}
 
 	private void hookContextMenu() {
@@ -207,5 +288,6 @@ public class EduRideJunitView extends ViewPart {
 	 */
 	public void setFocus() {
 		viewer.getControl().setFocus();
+		pagebook.setFocus();
 	}
 }
