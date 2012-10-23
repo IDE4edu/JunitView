372a45842ed914d70f82288cf68ca23e14fb3e66
Luke Lu <luke_lmn@berkeley.edu>
2012-10-22 18:40:45 -0700

    Added code to that finds individual JUnit Tests and displays them

--- trunk/TestProjects/TestProject1/src/SquareTest.java
+++ trunk/TestProjects/TestProject1/src/SquareTest.java
@@ -8,12 +8,13 @@ public class SquareTest {
 	@Test
 	public void testArea() {
 		Square x = new Square(5);
-		assertEquals("Square Area Failed", x.area(), 55);
+		assertEquals("Square Area Failed", x.area(), 25);
 	}
 	
 	@Test
 	public void testPerimeter() {
-		fail("Not yet implemented");
+		Square x = new Square(5);
+		assertEquals("Square Perimeter Failed", x.perimeter(), 20);
 	}
 
 }
