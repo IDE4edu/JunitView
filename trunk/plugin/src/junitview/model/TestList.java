d405ac56a6edd5c60c456bee79da92d917a05e97
Luke Lu <luke_lmn@berkeley.edu>
2013-02-25 15:21:30 -0800

    added testList and testResult

--- trunk/plugin/src/junitview/model/TestList.java
+++ trunk/plugin/src/junitview/model/TestList.java
@@ -1,11 +1,28 @@
 package junitview.model;
 
+import java.util.ArrayList;
+
+import org.eclipse.jdt.junit.model.ITestElement;
 import org.eclipse.jdt.junit.model.ITestRunSession;
 
 public class TestList {
+	
+	public ArrayList<TestResult> test_results;
 
 	public TestList(ITestRunSession session) {
-		// TODO Auto-generated constructor stub
+		test_results = new ArrayList<TestResult>();
+		ITestElement[] testElements = session.getChildren();
+		for (int i= 0; i < testElements.length; i++){
+			ITestElement x = testElements[i];
+			test_results.add(new TestResult(x.getProgressState().toString(), x.getTestResult(false).toString(), x.getFailureTrace().toString()));
+		}
+		
+		// ITestRunSession doesn't have squat
+		// ITestRunElement has an array of ITestElements
+		// ITestElement has shit we need!
+		
+		// TODO build test results into here based on constructor
+		
 	}
 
 }
--- trunk/plugin/src/junitview/model/TestResult.java
+++ trunk/plugin/src/junitview/model/TestResult.java
@@ -1,5 +1,35 @@
 package junitview.model;
 
-public class TestResult {
+import org.eclipse.jdt.junit.model.ITestElement;
+import org.eclipse.jdt.junit.model.ITestElementContainer;
+import org.eclipse.jdt.junit.model.ITestRunSession;
+import org.eclipse.jdt.junit.model.ITestElement.FailureTrace;
+import org.eclipse.jdt.junit.model.ITestElement.ProgressState;
+import org.eclipse.jdt.junit.model.ITestElement.Result;
 
+public class TestResult {
+	
+	private String progress_state;
+	
+	private String result;
+	
+	private String failure_trace;
+	
+	public TestResult(String progress_state, String result, String failure_trace){
+		this.progress_state = progress_state;
+		this.result = result;
+		this.failure_trace = failure_trace;
+	}
+	
+	public String get_progress_state(){
+		return progress_state;
+	}
+	
+	public String get_result(){
+		return result;
+	}
+	
+	public String get_failure_trace(){
+		return failure_trace;
+	}
 }
