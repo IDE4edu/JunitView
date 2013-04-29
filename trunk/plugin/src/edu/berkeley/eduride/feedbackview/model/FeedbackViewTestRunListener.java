7310095325640c3e9ecf5f56f4b12ac269cd673f
Nathaniel Titterton <nate@berkeley.edu>
2013-04-29 15:46:09 -0700

    draft of testliststore, needs new testlist constructor

--- trunk/plugin/src/edu/berkeley/eduride/feedbackview/model/FeedbackViewTestRunListener.java
+++ trunk/plugin/src/edu/berkeley/eduride/feedbackview/model/FeedbackViewTestRunListener.java
@@ -2,21 +2,10 @@ package edu.berkeley.eduride.feedbackview.model;
 
 
 
-import navigatorView.NavigatorActivator;
-
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IWorkspace;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.internal.junit.ui.JUnitPlugin;
+import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.junit.TestRunListener;
-import org.eclipse.jdt.junit.model.ITestCaseElement;
 import org.eclipse.jdt.junit.model.ITestRunSession;
-import org.eclipse.debug.core.ILaunchConfiguration;
-import org.eclipse.jdt.internal.junit.model.*;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
+
 
 import edu.berkeley.eduride.feedbackview.EduRideFeedback;
 
@@ -31,21 +20,15 @@ public class FeedbackViewTestRunListener extends TestRunListener {
 
 	public void sessionStarted(ITestRunSession session) {
 		EduRideFeedback.asyncShowFeedbackView();
-		System.out.println("sessionname: "+ session.getTestRunName());
-
-//		IWorkspace workspace = ResourcesPlugin.getWorkspace();
-//
-//		IProject[] projects = workspace.getRoot().getProjects();
-//
-//		for (IProject project : projects) {
-//			if (project.getName().equals("curriculum libraries")) {
-//				new ASTparse(project, "SquareTest.java").getSource();
-//			}
-//		}
+		IJavaProject proj = session.getLaunchedProject();
+		String javaSourceName = session.getTestRunName() + ".java";   // sigh - junit doesn't give us the package, so we have to fudge
+		
+		// IJavaElement.isStructureKnown will tell if this is a class file or a source fi
+		//new ASTparse("curriculum libraries", "SquareTest.java").getSource();
 	}
-
+	
 	public void sessionFinished(ITestRunSession session) {
-		ILaunchConfiguration configuraton = NavigatorActivator.getLastLaunchConfiguration();
+		//ILaunchConfiguration configuraton = NavigatorActivator.getLastLaunchConfiguration();
 		TestList tl = new TestList(session);
 		EduRideFeedback.getDefault().asyncupdateTests(tl);
 	}
